59ee36a27b9cb254ae0c124436ea1ec1af5fd876
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-07-15 22:21:47 +0545

    final

--- src/http/index.js
+++ src/http/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3500";
 // unauthenticated user register/login
 const API = axios.create({
-    baseURL : "http://localhost:3500/api",
+    baseURL : API_BASE_URL + "/api",
     headers:{
         "Content-Type":"application/json",            // if form,image use multipart/formData
         "Accept":"application/json"
@@ -12,7 +12,7 @@ const API = axios.create({
 
 // authenticated user (giving permision)
 const APIAuthenticated = axios.create({
-    baseURL : "http://localhost:3500/api",
+    baseURL : API_BASE_URL + "/api",
     headers:{
         "Content-Type":"application/json",            // if form,image use multipart/formData
         "Accept":"application/json",
